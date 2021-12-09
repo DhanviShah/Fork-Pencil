6b78b62cc528aacf435b8dc97fed1b53ffc3b3e2
Dhanvi Shah <45822642+DhanviShah@users.noreply.github.com>
2021-12-09 18:02:36 +0530

    Adding App files

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nutritions from "./components/Nutritions";
+import Recipes from "./components/Recipes";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Create from "./components/Create";
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/nutritions" element={<Nutritions />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+        {/* <Nutritions /> */}
+        {/* <Recipes /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
